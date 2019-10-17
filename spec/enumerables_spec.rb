36cec13763c86b3c538931997b234d4deb9d2ee9
Maryfel Mendoza <maryfelmendoza@hotmail.com>
2019-10-17 11:54:41 -0500

    enumerables spec created

---
+++ spec/enumerables_spec.rb
@@ -0,0 +1,19 @@
+describe 'Enumerable' do
+  describe '.my_each' do
+    it ""
+    expect().to
+    end
+  end
+
+  describe '.my_each_with_index' do
+    it ""
+    expect().to
+    end
+  end
+
+  describe '.my_select' do
+    it ""
+    expect().to
+    end
+  end
+end
